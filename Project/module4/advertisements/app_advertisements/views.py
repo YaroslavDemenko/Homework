e79fcb4ae5ccaf13b59e5902751eefa21cc13769
YaroslavDemenko <140470713+YaroslavDemenko@users.noreply.github.com>
2023-08-26 11:11:49 +0900

    Add files via upload

---
+++ Project/module4/advertisements/app_advertisements/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
+
+
+def index(requests):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(requests, 'index.html', context)
+
+def top_sellers(requests):
+    return render(requests, 'top_sellers.html')
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement=Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context ={'form': form}
+    return render(request, 'advertisement-post.html', context)
